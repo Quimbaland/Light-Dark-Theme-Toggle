0e5ca696115a538a257841625d57a16bce3a631d
Quimbaland <bquimby88@gmail.com>
2024-07-25 12:46:11 -0400

    Final commit

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
-// "use strict";
+"use strict";
+
+const sunMoonContainer = document.querySelector('.sun-moon-container');
 
 document.querySelector('.theme-toggle-button').addEventListener('click', () => {
-    console.log('Button clicked');
     document.body.classList.toggle('dark');
+    const currentRotation = parseInt(getComputedStyle(sunMoonContainer).getPropertyValue('--rotation'));
+    sunMoonContainer.style.setProperty('--rotation', currentRotation + 180);
 });
